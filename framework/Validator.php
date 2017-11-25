188a176ecd4cab8cfab9e6dac9a1fd43e0f650aa
raoyc <raoyc2009@gmail.com>
2017-11-25 23:15:31 +0800

    update validation service

--- framework/Validator.php
+++ framework/Validator.php
@@ -24,29 +24,16 @@ class Validator
     public static function make($data, $rules)
     {
         if (self::$factory === null) {
-            $translator = new Translator();
-            $instanace = $translator->getInstance();
+            $instanace = app('translator')->getInstance();
             $factory = new Factory($instanace);
-
-            $driver = config('database.default', 'mysql');
-            $database = config('database.connections.'.$driver);
-            $connection = new DB();
-            $connection->addConnection($database);
-            // $connection->bootEloquent();
-            // $connection->setAsGlobal();
-            $factory->setPresenceVerifier(new DatabasePresenceVerifier($connection->getDatabaseManager()));
-
+            $factory->setPresenceVerifier(new DatabasePresenceVerifier(app('db')->getDatabaseManager()));
             // Validate cellphone number in mainland China
             $factory->extend('cellphone', function ($attribute, $value, $parameters, $validator) {
                 return preg_match('/^13[0-9]{9}|14[57]{1}[0-9]{8}|15[012356789]{1}[0-9]{8}|170[059]{1}[0-9]{8}|18[0-9]{9}|177[0-9]{8}$/', $value);
             });
-
             self::$factory = $factory;
-
         } else {
-
             $factory = self::$factory;
-
         }
 
         $validator = $factory->make($data, $rules);
