8a2ba4cf1ae741283876163478642417eb69be0e
raoyc <raoyc2009@gmail.com>
2017-11-25 23:37:31 +0800

    add more comments

--- framework/DB.php
+++ framework/DB.php
@@ -2,6 +2,13 @@
 
 namespace LightSwoole\Framework;
 
+/**
+ * Class DB
+ *
+ * @package LightSwoole\Framework
+ * @author raoyc <raoyc2009@gmaill.com>
+ * @link   https://raoyc.com
+ */
 class DB extends \Illuminate\Database\Capsule\Manager
 {
 
