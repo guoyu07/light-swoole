a8fc56490e975cfd87240b2efad4741f01dc5ec6
raoyc <raoyc2009@gmail.com>
2017-11-25 23:11:55 +0800

    add localization message

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -2,4 +2,6 @@
 
 return [
     'welcome' => 'Welcome to Light Swoole',
+    'apples' => 'There is one apple|There are many apples',
+    'bananas' => '{0} There are none|[1,19] There are some|[20,*] There are many',
 ];
--- resources/lang/zh-CN/message.php
+++ resources/lang/zh-CN/message.php
@@ -2,4 +2,6 @@
 
 return [
     'welcome' => '欢迎来到 Light Swoole',
+    'apples' => '这里有一个苹果|这里有很多苹果',
+    'bananas' => '{0} 没有|[1,19] 一些|[20,*] 很多',
 ];
